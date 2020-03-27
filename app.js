79d2d3afdf9bba90c03c955648dbdb6b8389dfd8
Teresa Cho <tcho@cca.edu>
2020-03-26 19:17:37 -0700

    creates a new object using new keyword

---
+++ app.js
@@ -0,0 +1,17 @@
+// Create a new object with direct instance (using new operator), and add some properties to that object. 
+const pokedex = new Object();
+pokedex.name = 'Bulbasaur';
+pokedex.category = 'Seed';
+pokedex.type = [
+  'Grass',
+  'Poison',
+];
+pokedex.weakness = [
+  'Fire',
+  'Flying',
+  'Ice',
+  'Psychic',
+];
+
+// console.log(pokedex);
+
